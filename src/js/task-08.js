c2f2b2c33682eeabc501a17b0aaea81daa5e8589
Ekaterina-Kazantseva <opanasenko_katerina@ukr.net>
2022-08-10 12:42:11 +0300

    Doing task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    console.log(mail, password)
+
+    if (mail === '' || password=== '') {
+        return alert('Must be filled')
+    }
+
+    event.currentTarget.reset()
+}
 
