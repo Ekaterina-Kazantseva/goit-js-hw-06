4896ba7aeb8947b901461138a1a80b578aa9af36
Ekaterina-Kazantseva <opanasenko_katerina@ukr.net>
2022-08-10 13:11:31 +0300

    Doing task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  changeBtn: document.querySelector('button.change-color'),
+  body: document.querySelector('body'),
+  colorValue: document.querySelector('span.color')
+}
+
+refs.changeBtn.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  refs.colorValue.textContent = color;
+})
